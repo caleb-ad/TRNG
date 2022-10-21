856a02c662357a4e8247ab2da734c2017b618957
Caleb Dennis <caleb.dennis429@gmail.com>
2022-10-20 19:41:56 -0700

    use single trng sequentially

--- sources_1/new/rng_io.sv
+++ sources_1/new/rng_io.sv
@@ -10,23 +10,26 @@ module rng_io(
     parameter amount_rng_bits = 16;
     
     logic [amount_rng_bits - 1:0] rng_value;
-    logic [amount_rng_bits - 1:0] rng_bit;
+    logic [3:0] rng_count;
+    logic rng_bit;
     logic enable, ack, rng_bit, bit_ready;
     
-    generate
-        for(genvar i=0; i<amount_rng_bits; i++) trng(.EN(enable), .RANDOM(rng_bit[i]), .BIT_READY(bit_ready), .ACK(ack));
-    endgenerate
-    
+  
+   TRNG trng(.EN(enable), .RANDOM(rng_bit), .BIT_READY(bit_ready), .ACK(ack));
+   
     always_ff @(posedge CLK) begin
         ack <= 0;
         
         if(BTNC) begin
             rng_value <= 0;
             enable <= 1;
+            rng_count <= 0;
         end else if(bit_ready) begin
-            rng_value <= rng_bit;
+            rng_value[rng_count] <= rng_bit;
             ack <= 1;
+            rng_count <= rng_count + 1;
         end
+        
         else if(bit_ready) enable <= 0;
     end
     
