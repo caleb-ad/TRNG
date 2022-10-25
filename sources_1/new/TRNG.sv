ee907a5150d890028cee62eb76c47a079fa8e876
Tyler Verdi <tverdi@calpoly.edu>
2022-10-25 10:28:09 -0700

    fixed rng_io and cleared count

--- sources_1/new/TRNG.sv
+++ sources_1/new/TRNG.sv
@@ -8,10 +8,13 @@
     );
     
     parameter CHALLENGE = 13;
+    parameter min_samples = 1;
     
     logic CLK1, CLK2;
     logic sample;
     logic count;
+    //logic [3:0] sample_count = 4'b0000;
+    //logic sample_count = 0;
     
     ring_oscillator ro1( .EN(EN), .CHALLENGE(CHALLENGE), .OUT(CLK1));
     ring_oscillator ro2( .EN(EN), .CHALLENGE(CHALLENGE), .OUT(CLK2));
@@ -19,14 +22,18 @@
     always_ff @(posedge CLK1) begin
         sample <= CLK2;
         count <= ~count;
+//        if(!CLK2) begin 
+//            sample_count <= sample_count + 1;
+//        end
     end
     
     always_ff @(posedge sample, posedge ACK) begin
-        if(sample) begin
+        if(sample) begin // && (sample_count >= min_samples)
             RANDOM <= count;
+            count <= 0;
             BIT_READY <= 1;
         end
-        else if(ACK) BIT_READY <= 0; 
+        else if(ACK) BIT_READY <= 0; //if the ACK is too fast BIT_READY will never be reset 
     end
     
     
--- sources_1/new/rng_io.sv
+++ sources_1/new/rng_io.sv
@@ -11,11 +11,17 @@ module rng_io(
     
     logic [amount_rng_bits - 1:0] rng_value;
     logic [amount_rng_bits - 1:0] rng_bit;
-    logic enable, ack, rng_bit, bit_ready;
+    logic [amount_rng_bits - 1:0] rng_bit_ready;
+    logic enable, ack, bit_ready;
     
     generate
-        for(genvar i=0; i<amount_rng_bits; i++) trng(.EN(enable), .RANDOM(rng_bit[i]), .BIT_READY(bit_ready), .ACK(ack));
+        for(genvar i=0; i<amount_rng_bits; i++) TRNG(.EN(enable), .RANDOM(rng_bit[i]), .BIT_READY(rng_bit_ready[i]), .ACK(ack));
     endgenerate
+    
+    always_comb begin
+        if(&rng_bit_ready == 1) bit_ready <= 1;
+        else bit_ready <= 0;
+    end
     
     always_ff @(posedge CLK) begin
         ack <= 0;
@@ -26,8 +32,8 @@ module rng_io(
         end else if(bit_ready) begin
             rng_value <= rng_bit;
             ack <= 1;
+            enable <= 0;
         end
-        else if(bit_ready) enable <= 0;
     end
     
     sseg_des dec (
