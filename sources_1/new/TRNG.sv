b2c07c4bd9a97f01653dfec2c827745938619a93
Caleb Dennis <caleb.dennis429@gmail.com>
2022-10-26 09:51:42 -0700

    implemented counter for TRNG

--- sources_1/new/TRNG.sv
+++ sources_1/new/TRNG.sv
@@ -8,13 +8,13 @@
     );
     
     parameter CHALLENGE = 13;
-    parameter min_samples = 1;
+    parameter min_samples = 31; //idk
     
     logic CLK1, CLK2;
     logic sample;
     logic count;
-    //logic [3:0] sample_count = 4'b0000;
-    //logic sample_count = 0;
+    logic [4:0] sample_count = 0;
+    logic sample_count_done;
     
     ring_oscillator ro1( .EN(EN), .CHALLENGE(CHALLENGE), .OUT(CLK1));
     ring_oscillator ro2( .EN(EN), .CHALLENGE(CHALLENGE), .OUT(CLK2));
@@ -22,19 +22,21 @@
     always_ff @(posedge CLK1) begin
         sample <= CLK2;
         count <= ~count;
-        if(BIT_READY) count <= 0;
-//        if(!CLK2) begin 
-//            sample_count <= sample_count + 1;
-//        end
+        if(BIT_READY) begin
+            sample_count <= 0;
+            sample_count_done <= 0;
+            count <= 0;
+        end
+        if(sample_count >= min_samples) sample_count_done <= 1;
+        else if(!sample_count_done) sample_count <= sample_count + 1; 
     end
     
     always_ff @(posedge sample, posedge ACK) begin
-        if(sample) begin // && (sample_count >= min_samples)
+        if(ACK) BIT_READY <= 0;
+        else if(sample && ~BIT_READY && sample_count_done) begin
             RANDOM <= count;
             BIT_READY <= 1;
         end
-        else if(ACK) BIT_READY <= 0; //if the ACK is too fast BIT_READY will never be reset 
     end
-    
     
 endmodule
--- sources_1/new/rng_io.sv
+++ sources_1/new/rng_io.sv
@@ -10,15 +10,15 @@ module rng_io(
     parameter amount_rng_bits = 16;
     
     logic [amount_rng_bits - 1:0] rng_value;
-    logic [3:0] rng_count;
+    logic [4:0] rng_count;
     logic enable, ack, rng_bit, bit_ready;
     
-  
-    TRNG trng(.EN(enable), .RANDOM(rng_bit), .BIT_READY(bit_ready), .ACK(ack));
+    TRNG get_rng(.EN(enable), .RANDOM(rng_bit), .BIT_READY(bit_ready), .ACK(ack));
    
     always_ff @(posedge CLK) begin
         ack <= 0;
         
+        if(rng_count >= 16) enable <= 0;
         if(BTNC) begin
             rng_value <= 0;
             enable <= 1;
@@ -28,8 +28,6 @@ module rng_io(
             ack <= 1;
             rng_count <= rng_count + 1;
         end
-        
-        else if(bit_ready) enable <= 0;
     end
     
     sseg_des dec (
