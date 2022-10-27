76573c80b81ad74e66d63cc1b943121291bff17e
Caleb Dennis <caleb.dennis429@gmail.com>
2022-10-27 11:33:59 -0700

    added bias corrector to TRNG

--- sources_1/new/TRNG.sv
+++ sources_1/new/TRNG.sv
@@ -15,6 +15,7 @@
     logic count;
     logic [2:0] sample_count = 0;
     logic sample_count_done;
+    logic bias_correct, reset_count;
     
     ring_oscillator ro1( .EN(EN), .CHALLENGE(CHALLENGE), .OUT(CLK1));
     ring_oscillator ro2( .EN(EN), .CHALLENGE(CHALLENGE), .OUT(CLK2));
@@ -22,7 +23,7 @@
     always_ff @(posedge CLK1) begin
         sample <= CLK2;
         count <= ~count;
-        if(BIT_READY) begin
+        if(reset_count) begin
             sample_count <= 0;
             sample_count_done <= 0;
             count <= 0;
@@ -36,10 +37,18 @@
     end
     
     always_ff @(posedge sample, posedge ACK) begin
+        reset_count <= 0;
         if(ACK) BIT_READY <= 0;
-        else if(sample && ~BIT_READY && sample_count_done) begin
-            RANDOM <= count;
-            BIT_READY <= 1;
+        else if(sample && sample_count_done && ~BIT_READY) begin
+            reset_count <= 1;
+            if(~bias_correct) begin
+                RANDOM <= count;
+                bias_correct <= 1;
+            end else begin
+                RANDOM <= RANDOM ^ count;
+                BIT_READY <= 1;
+                bias_correct <= 0;
+            end
         end
     end
     
