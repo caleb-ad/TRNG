fb03fb65f77f6b4b36687c55e929919fc4a0d547
Tyler Verdi <tverdi@calpoly.edu>
2022-10-26 18:45:19 -0700

    XORs two random bits collected back to back

--- sources_1/new/TRNG.sv
+++ sources_1/new/TRNG.sv
@@ -8,12 +8,13 @@
     );
     
     parameter CHALLENGE = 13;
-    parameter min_samples = 3;
+    parameter min_samples = 6;
     
     logic CLK1, CLK2;
     logic sample;
     logic count;
     logic [3:0] sample_count;
+    logic prev_rand_bit;
     
     ring_oscillator ro1( .EN(EN), .CHALLENGE(CHALLENGE), .OUT(CLK1));
     ring_oscillator ro2( .EN(EN), .CHALLENGE(CHALLENGE), .OUT(CLK2));
@@ -25,18 +26,22 @@
             count <= 0;
             sample_count = 0;
         end
-        if(!CLK2) begin 
-            sample_count <= sample_count + 1;
-        end
+        else if(sample_count == min_samples / 2) count <= 0;
+//        if(~CLK2) begin 
+//            sample_count <= sample_count + 1;
+//        end
+        sample_count <= sample_count + 1;
     end
     
     always_ff @(posedge sample, posedge ACK) begin
         if(ACK) BIT_READY <= 0;
-        else if((sample & ~BIT_READY) && (sample_count > min_samples)) begin // && (sample_count >= min_samples)
-            RANDOM <= count;
+        else if ((sample & ~BIT_READY) && (sample_count == (min_samples / 2))) begin
+            prev_rand_bit <= count;
+        end
+        else if((sample & ~BIT_READY) && (sample_count > min_samples)) begin // && prev_rand_bit_flag
+            RANDOM <= count ^ prev_rand_bit; 
             BIT_READY <= 1;
         end
     end
-    
-    
+      
 endmodule
